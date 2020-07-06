7493683e46ff9250c471804c3db62460d307284a
Marcelo Ferrão Feodrippe <marcelo@feodrippe.com>
2020-07-05 21:34:11 -0300

    Initial Commit

---
+++ GerenciamentoPontos/Core/GerenciadorPontos.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoFisica.Pontos.Core
+{
+    public class GerenciadorPontos
+    {
+        private int _conjuntoAtual;
+        private IList<Vector2> _pontosAtuais;
+        private readonly IDictionary<int, IList<Vector2>> _conjuntoPontos;
+
+        public GerenciadorPontos()
+        {
+            _conjuntoAtual = 0;
+            _conjuntoPontos = new Dictionary<int, IList<Vector2>>();
+            MudarConjunto(_conjuntoAtual);
+        }
+
+        public int ConjuntoAtual { get { return _conjuntoAtual; } private set { } }
+
+        public IEnumerable<int> ConjuntosAtuais()
+        { 
+            foreach(var conjunto in _conjuntoPontos)
+            {
+                yield return conjunto.Key;
+            }
+        }
+
+        public IEnumerable<ISet<Vector2>> TodosPontos()
+        {
+            foreach (var conjunto in _conjuntoPontos)
+            {
+                yield return new HashSet<Vector2>(conjunto.Value);
+            }
+        }
+
+        public ISet<Vector2> PontosAtuais()
+        {
+            return new HashSet<Vector2>(_pontosAtuais);
+        }
+
+        public void MudarConjunto(int conjunto)
+        {
+            _conjuntoAtual = conjunto;
+
+            if (_conjuntoPontos.ContainsKey(_conjuntoAtual))
+            {
+                _pontosAtuais = _conjuntoPontos[_conjuntoAtual];
+            }
+            else
+            {
+                _pontosAtuais = new List<Vector2>();
+            }
+
+        }
+
+        public void AdicionarPonto(Vector2 ponto)
+        {
+            _pontosAtuais.Add(ponto);
+
+            if (!_conjuntoPontos.ContainsKey(_conjuntoAtual))
+            {
+                _conjuntoPontos[_conjuntoAtual] = _pontosAtuais;
+            }
+        }
+
+        public void AdicionarPontos(IEnumerable<Vector2> pontos)
+        {
+            foreach (Vector2 ponto in pontos)
+            {
+                AdicionarPonto(ponto);
+            }
+        }
+
+        public void RemoverPonto(Vector2 ponto)
+        {
+            _pontosAtuais.Remove(ponto);
+
+            if (_conjuntoPontos.ContainsKey(_conjuntoAtual) && !_pontosAtuais.Any())
+            {
+                _conjuntoPontos.Remove(_conjuntoAtual);
+            }
+        }
+
+        public void RemoverPontos(IEnumerable<Vector2> pontos)
+        {
+            foreach (Vector2 ponto in pontos)
+            {
+                RemoverPonto(ponto);
+            }
+        }
+
+        public void LimparPontos()
+        {
+            _pontosAtuais.Clear();
+
+            if (_conjuntoPontos.ContainsKey(_conjuntoAtual))
+            {
+                _conjuntoPontos.Remove(_conjuntoAtual);
+            }
+        }
+
+        public float DistanciaTotalConjuntoPontos(int conjunto)
+        {
+            float distancia = 0;
+
+            if (_conjuntoPontos.ContainsKey(conjunto) && _conjuntoPontos[conjunto].Count > 1)
+            {
+                var pontos = _conjuntoPontos[conjunto];
+
+                Vector2 p1 = _pontosAtuais.First();
+
+                for (int i = 1; i < _pontosAtuais.Count; i++)
+                {
+                    Vector2 p2 = pontos[i];
+
+                    distancia += DistanciaPontos(p1, p2);
+
+                    p1 = p2;
+                }
+            }
+            return distancia;
+        }
+
+        public float DistanciaMediaConjuntoPontos(int conjunto)
+        {
+            float media = 0;
+
+            if (_conjuntoPontos.ContainsKey(conjunto) && _conjuntoPontos[conjunto].Count > 1)
+            {
+                media = DistanciaTotalConjuntoPontos(conjunto) / _conjuntoPontos[conjunto].Count;
+            }
+
+            return media;
+        }
+
+        public float DistanciaPontos(Vector2 p1, Vector2 p2)
+        {
+            return Vector2.Distance(p1, p2);
+        }
+    }
+}
---
+++ UPrincipal/UPrincipalForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace UPrincipal
+{
+    partial class UPrincialForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UPrincialForm));
+            this.MenuPrincial = new System.Windows.Forms.MenuStrip();
+            this.açõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.carregarMóduloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Visor = new System.Windows.Forms.PictureBox();
+            this.MenuPrincial.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Visor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MenuPrincial
+            // 
+            this.MenuPrincial.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.açõesToolStripMenuItem,
+            this.opçõesToolStripMenuItem});
+            resources.ApplyResources(this.MenuPrincial, "MenuPrincial");
+            this.MenuPrincial.Name = "MenuPrincial";
+            // 
+            // açõesToolStripMenuItem
+            // 
+            this.açõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.carregarMóduloToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.açõesToolStripMenuItem.Name = "açõesToolStripMenuItem";
+            resources.ApplyResources(this.açõesToolStripMenuItem, "açõesToolStripMenuItem");
+            // 
+            // carregarMóduloToolStripMenuItem
+            // 
+            this.carregarMóduloToolStripMenuItem.Name = "carregarMóduloToolStripMenuItem";
+            resources.ApplyResources(this.carregarMóduloToolStripMenuItem, "carregarMóduloToolStripMenuItem");
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            resources.ApplyResources(this.sairToolStripMenuItem, "sairToolStripMenuItem");
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // opçõesToolStripMenuItem
+            // 
+            this.opçõesToolStripMenuItem.Name = "opçõesToolStripMenuItem";
+            resources.ApplyResources(this.opçõesToolStripMenuItem, "opçõesToolStripMenuItem");
+            // 
+            // Visor
+            // 
+            this.Visor.Cursor = System.Windows.Forms.Cursors.Default;
+            resources.ApplyResources(this.Visor, "Visor");
+            this.Visor.Name = "Visor";
+            this.Visor.TabStop = false;
+            // 
+            // UPrincialForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.Visor);
+            this.Controls.Add(this.MenuPrincial);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MainMenuStrip = this.MenuPrincial;
+            this.Name = "UPrincialForm";
+            this.MenuPrincial.ResumeLayout(false);
+            this.MenuPrincial.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Visor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuPrincial;
+        private System.Windows.Forms.ToolStripMenuItem açõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem carregarMóduloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opçõesToolStripMenuItem;
+        private System.Windows.Forms.PictureBox Visor;
+    }
+}
+
